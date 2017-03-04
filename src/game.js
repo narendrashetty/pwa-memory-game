6e08b9c25edc5e31c9709fa057c3503f09dcc3d6
Narendra N Shetty <mailnarendra15@gmail.com>
2017-03-04 18:29:36 +0100

    wrote the logic of picking and matching the cards

--- src/game.js
+++ src/game.js
@@ -1,16 +1,44 @@
-const Game = function() {
+const Game = function () {
   this.$table = document.querySelector('.memory-game-table');
   this.rows = 6;
   this.cols = 4;
+  this.guess = null;
 };
 
-Game.prototype.init = function(cards) {
+Game.prototype.init = function (cards) {
   this.cards = this.shuffleCards(cards.concat(cards));
   const dom = this.buildDOM();
   this.$table.innerHTML = dom;
+  this.addListeners();
 };
 
-Game.prototype.buildDOM = function() {
+Game.prototype.addListeners = function () {
+  this.$table.addEventListener('click', this.cardClicked.bind(this));
+};
+
+Game.prototype.cardClicked = function (e) {
+  let $target = e.target;
+  if ($target.classList.contains('memory-game-cell')) {
+    if (!$target.classList.contains('matched') && !$target.classList.contains('picked')) {
+      $target.classList.add('picked');
+      if (!this.guess) {
+        this.guess = $target;
+        return;
+      }
+
+      if ($target.innerHTML === this.guess.innerHTML) {
+        this.guess.classList.add('matched');
+        $target.classList.add('matched');
+      }
+
+      this.guess.classList.remove('picked');
+      $target.classList.remove('picked');
+      this.guess = null;
+    }
+  }
+};
+
+Game.prototype.buildDOM = function () {
   var dom = '';
 
   for (let i = 0; i < this.rows; i++) {
@@ -26,7 +54,7 @@ Game.prototype.buildDOM = function() {
   return dom;
 };
 
-Game.prototype.shuffleCards = function(cards) {
+Game.prototype.shuffleCards = function (cards) {
   let len = cards.length;
 
   while (len > 0) {
